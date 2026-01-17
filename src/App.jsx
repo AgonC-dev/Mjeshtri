ced10f13b37a1674c4fb7b392779f3cb8cdeb39c
arrow123-web <liscitaku@gmail.com>
2026-01-17 04:14:47 +0100

    helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 // Layouts & Components
 import MainLayout from "./components/MainLayout";
