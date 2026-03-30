80054568b3c158d1dd10f6bb191a866354cb7ad6
arrow123-web <liscitaku@gmail.com>
2026-03-30 15:23:55 +0200

    new chnages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 import { useEffect } from 'react';
-import Logo from '../../assets/FooterLogo.png'
+import Logo from '../../assets/FooterLogo2.png'
 
 function Footer() {
 
